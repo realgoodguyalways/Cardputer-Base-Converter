8f7c128721994b77dcaa97792bb2e88ed3de5bc5
Kai <115123479+realgoodguyalways@users.noreply.github.com>
2026-07-28 13:15:28 -0500

    connected buttons & display, and programmed the UI
    I haven't started the base converting logic, but the UI for the screen is pretty much complete.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -6,25 +6,108 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+String output = "0";
+String bases[3] = {"Decimal", "Binary", "Octal"};
+int x = 0;
+
+int minBases = 0;
+int maxBases = 2;
+
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once:99
   Serial.begin(115200);
   Wire.begin(1, 2);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
 
+  pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
+  pinMode(18, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(42, INPUT_PULLUP);
+  pinMode(41, INPUT_PULLUP);
+  pinMode(40, INPUT_PULLUP);
+  pinMode(39, INPUT_PULLUP);
 
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.println("hello World");
+  display.println("Decimal <");
+  display.println("Binary");
+  display.println("Octal");
   display.display();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  delay(1000); // this speeds up the simulation
+  if (digitalRead(4) == 0) {
+    output = output + "9";
+  } else if (digitalRead(5) == 0) {
+    output = output + "8";
+  } else if (digitalRead(6) == 0) {
+    output = output + "7";
+  } else if (digitalRead(7) == 0) {
+    output = output + "6";
+  } else if (digitalRead(15) == 0) {
+    output = output + "5";
+  } else if (digitalRead(16) == 0) {
+    output = output + "4";
+  } else if (digitalRead(17) == 0) {
+    output = output + "3";
+  } else if (digitalRead(18) == 0) {
+    output = output + "2";
+  } else if (digitalRead(8) == 0) {
+    output = output + "1";
+  } else if (digitalRead(3) == 0) {
+    output = output + "0";
+  } else if (digitalRead(41) == 0) {
+    output.remove(output.length() - 1);
+  }else if (digitalRead(42) == 0) {
+
+  }else if (digitalRead(40) == 0) {
+    changeBase(1);
+  }else if (digitalRead(39) == 0) {
+    changeBase(0);
+  }
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    //display.println(bases[x] + "< " + output);
+    display.println(bases[0]);
+    display.println(bases[1]);
+    display.println(bases[2]);
+    if (x == 0) {
+      display.setCursor(40, 0);
+      display.println(":" + output);
+    }else if (x == 1) {
+      display.setCursor(35, 9);
+      display.println(":" + output);
+    }else {
+      display.setCursor(28, 17);
+      display.println(":" + output);
+    }
+    display.display();
+
+  delay(40);
+}
+
+void changeBase(int y) {
+  if(y == 0) {
+    if(x != minBases){
+      x--;
+    }
+  }else if(y == 1) {
+    if(x != maxBases){
+      x++;
+    }
+  }
 }
