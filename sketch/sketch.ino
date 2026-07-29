d9f5740babfe537f99adff44102d371488bbe61c
Kai <115123479+realgoodguyalways@users.noreply.github.com>
2026-07-28 19:42:26 -0500

    added easy num conversions && ui updates
    I added arrow to point to which base you are converting to, and I added the easy conversions(other bases to decimal)

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,17 +1,29 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <iostream>
+#include <map>
 
 #define SCREEN_HEIGHT 64
 #define SCREEN_WIDTH 128
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-String output = "0";
+String output = "";
+String outputDeci = "0";
+String outputOct = "0";
+String outputBin = "0";
+String var1 = ": ";
+String var2 = "";
+String var3 = "";
 String bases[3] = {"Decimal", "Binary", "Octal"};
 int x = 0;
+int base = 0;
+boolean changed = true;
+
 
 int minBases = 0;
 int maxBases = 2;
+
 
 void setup() {
   // put your setup code here, to run once:99
@@ -47,21 +59,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(4) == 0) {
+  if (digitalRead(4) == 0 && (base == 0)) {
     output = output + "9";
-  } else if (digitalRead(5) == 0) {
+  } else if (digitalRead(5) == 0 && (base == 0)) {
     output = output + "8";
-  } else if (digitalRead(6) == 0) {
+  } else if (digitalRead(6) == 0 && (base == 0 || base == 2)) {
     output = output + "7";
-  } else if (digitalRead(7) == 0) {
+  } else if (digitalRead(7) == 0 && (base == 0 || base == 2)) {
     output = output + "6";
-  } else if (digitalRead(15) == 0) {
+  } else if (digitalRead(15) == 0 && (base == 0 || base == 2)) {
     output = output + "5";
-  } else if (digitalRead(16) == 0) {
+  } else if (digitalRead(16) == 0 && (base == 0 || base == 2)) {
     output = output + "4";
-  } else if (digitalRead(17) == 0) {
+  } else if (digitalRead(17) == 0 && (base == 0 || base == 2)) {
     output = output + "3";
-  } else if (digitalRead(18) == 0) {
+  } else if (digitalRead(18) == 0 && (base == 0 || base == 2)) {
     output = output + "2";
   } else if (digitalRead(8) == 0) {
     output = output + "1";
@@ -70,7 +82,7 @@ void loop() {
   } else if (digitalRead(41) == 0) {
     output.remove(output.length() - 1);
   }else if (digitalRead(42) == 0) {
-
+    change();
   }else if (digitalRead(40) == 0) {
     changeBase(1);
   }else if (digitalRead(39) == 0) {
@@ -82,19 +94,39 @@ void loop() {
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
     //display.println(bases[x] + "< " + output);
-    display.println(bases[0]);
-    display.println(bases[1]);
-    display.println(bases[2]);
-    if (x == 0) {
-      display.setCursor(40, 0);
-      display.println(":" + output);
-    }else if (x == 1) {
-      display.setCursor(35, 9);
-      display.println(":" + output);
+    display.println(bases[0] + var1 + outputDeci);
+    display.println(bases[1] + var2 + outputBin);
+    display.println(bases[2] + var3 + outputOct);
+
+    if (base == 0) {
+      outputDeci = output;
+      outputBin = "";
+      outputOct = "";
+
+      var1 = ": ";
+      var2 = "";
+      var3 = "";
+      vars();
+    }else if (base == 1) {
+      outputDeci = "";
+      outputBin = output;
+      outputOct = "";
+
+      var1 = "";
+      var2 = ": ";
+      var3 = "";
+      vars();
     }else {
-      display.setCursor(28, 17);
-      display.println(":" + output);
-    }
+      outputDeci = "";
+      outputBin = "";
+      outputOct = output;
+
+      var1 = "";
+      var2 = "";
+      var3 = ": ";
+      vars();
+    } 
+
     display.display();
 
   delay(40);
@@ -108,6 +140,58 @@ void changeBase(int y) {
   }else if(y == 1) {
     if(x != maxBases){
       x++;
+    }
+  }
+}
+
+void change() {
+  int decimal = 0;
+  int power = 1;
+  int oldBase = base;
+  base = x;
+
+  if  (oldBase == 0) {
+    if (base == 1) { //decimal to binary
+
+    } else { //decimal to octal
+
+    }
+  } else if (oldBase == 1) {
+    if (base == 0) { //binary to decimal
+
+      for (int i = output.length() - 1; i >= 0; i--) {
+        if (output[i] == '1') {
+          decimal += power;
+        }
+        power *= 2;
+      }
+
+      output = decimal;
+    } else { //binary to octal
+      
+    }
+  } else {
+    if (base == 0) { //octal to decimal
+      for (int i = output.length(); i >= 0; i--) {
+        decimal += (output[i] - '0') * power;
+        power *= 8;
+      }
+
+      output = decimal;
+    } else { //octal to binary
+      
+    }
+  }
+}
+
+void vars() {
+  if (x != base) {
+    if (x == 0) {
+      var1 = " <";
+    }else if (x == 1) {
+      var2 = " <";
+    }else {
+      var3 = " <";
     }
   }
 }
